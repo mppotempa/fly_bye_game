7642967b94d18ec5929f717230411a337ffecb40
mappotem <mappotem@iu.edu>
2019-11-14 15:07:30 -0500

    player can control ship in window

--- fly_bye/Assets/Scripts/PlayerController.cs
+++ fly_bye/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
+        rb.velocity = movement * speed;
     }
 }
