92b555df5acbb6d86a624793b52023f5fa1839a4
Mary Potempa <mappotem@iu.edu>
2019-11-19 13:41:48 -0500

    basic tutorial functionality done

--- fly_bye/Assets/Scripts/GameController.cs
+++ fly_bye/Assets/Scripts/GameController.cs
@@ -6,19 +6,34 @@ public class GameController : MonoBehaviour
 {
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnWaves();
+        StartCoroutine(SpawnWaves());
     }
 
     // Update is called once per frame
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for(int i=0; i< hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+        
 
     }
 }
