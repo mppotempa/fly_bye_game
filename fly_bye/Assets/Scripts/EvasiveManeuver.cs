2f0e091730b5d93a5d5f5d714e676dee19b42f98
mappotem <mappotem@iu.edu>
2019-11-21 15:44:11 -0500

    working on making enemy maneuver around screen

---
+++ fly_bye/Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour
+{
+
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    //public Boundary boundary;
+
+    private float currentSpeed;
+    private float targetManeuver;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+    }
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+        while (true)
+        {
+            //toDO
+
+        }
+    }
+}
